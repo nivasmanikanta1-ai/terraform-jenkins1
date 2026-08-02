d95d445af531047b37fb9c3b8019e340814482b6
nagswe <126598198+nagswe@users.noreply.github.com>
2026-08-02 20:39:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ provider "aws" {
 
 # Create a VPC
 resource "aws_instance" "web_server" {
-  ami           = "ami-0440d3b780d96b29d" # Replace with the desired AMI ID
+  ami           = "ami-0224ce6f9504665ee" # Replace with the desired AMI ID
   instance_type = var.ec2_instance_type
   tags = {
     Name = "web-server"
